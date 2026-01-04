e302228c0dc4f89d73d05e68bc530b03c1ca5cf5
Vansh Gupta <vanshgpt2911@gmail.com>
2026-01-04 20:20:32 +0530

    Clean up ignored files and datasets

--- .ipynb_checkpoints/compress_similarity-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import gzip
-
-with open("similarity.pkl", "rb") as f:
-    similarity = pickle.load(f)
-
-with gzip.open("similarity.pkl.gz", "wb") as f:
-    pickle.dump(similarity, f)
